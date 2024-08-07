66d097558a0841cbb12a1cec18b79b71de198f91
missn0body <hulk128t@gmail.com>
2024-08-07 04:59:14 +0000

    General rotating completed, albeit bugged

--- 1.0.5/rot.c
+++ 1.0.5/rot.c
@@ -1,3 +1,5 @@
+// TODO files not being accepted, wrong diagnostic output?
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,6 +41,7 @@ static constexpr short alpha_span  = (upper_end - upper_begin) + 1;
 // You can easily delete these and substitute for the actual functions
 static inline bool my_isgraph(const int what) { return ( what >= ascii_begin && what <= ascii_end ) ? true : false; }
 static inline bool my_isdigit(const int what) { return ( (what & 7) <= 9 && (what & 7) >= 0 ) ? true : false; }
+static inline bool strempty(const char *what) { return (*what == '\0'); }
 static inline const char *boolstr(bool what)  { return (what) ? "true" : "false"; }
 
 // Actual rotation function
@@ -46,7 +49,7 @@ static inline const char *boolstr(bool what)  { return (what) ? "true" : "false"
 // spitting out its current state and arguments to stdout
 void rotate(const char *in, const char *out, short shift, bool verbose)
 {
-	if(verbose) printf("%s: begin function (from %s, to %s, shift %d, verbose %s)", __func__, in, out, shift, boolstr(verbose));
+	if(verbose) printf("%s: begin function (from %s, to %s, shift %d, verbose %s)\n", __func__, in, out, shift, boolstr(verbose));
 
 	// We can either gather input from stdin or a file, and output to stdout or a file
 	FILE *in_obj  = (in  == nullptr) ? stdin  : fopen(in, "r");
@@ -57,7 +60,7 @@ void rotate(const char *in, const char *out, short shift, bool verbose)
 	if(shift >= 26) shift -= 26;
 	if(shift >= 52)
 	{
-		fprintf(stderr, "%s: enormous shift amount recieved, reverting to rot47...", __func__);
+		fprintf(stderr, "%s: enormous shift amount recieved, reverting to rot47...\n", __func__);
 		shift = 0;
 	}
 
@@ -69,14 +72,14 @@ void rotate(const char *in, const char *out, short shift, bool verbose)
 		if(shift == 0)
 		{
 			// ROT47
-			if(verbose) printf("%s: rot47 mode. begin rotation", __func__);
+			if(verbose) printf("%s: rot47 mode. begin rotation\n", __func__);
 			for(size_t i = 0; buffer[i] != '\0'; i++)
 				if(my_isgraph(buffer[i])) buffer[i] = ascii_begin + ((buffer[i] + 14) % ascii_span);
 		}
 		else if(shift != 0)
 		{
 			// ROT(N), e.g. ROT13, ROT1, ROT25, etc...
-			if(verbose) printf("%s: rot-n mode. begin rotation", __func__);
+			if(verbose) printf("%s: rot-n mode. begin rotation\n", __func__);
 			for(size_t i = 0; buffer[i] != '\0'; i++)
 			{
 				if(buffer[i] >= upper_begin && buffer[i] <= upper_end)
@@ -91,11 +94,11 @@ void rotate(const char *in, const char *out, short shift, bool verbose)
 		}
 
 		fprintf(out_obj, "%s\n", buffer);
-		if(verbose) printf("%s: done.", __func__);
+		if(verbose) printf("%s: done.\n", __func__);
 	}
 
 	// Clean up
-	if(verbose) printf("%s: end function (from %s, to %s, shift %d, verbose %s)", __func__, in, out, shift, boolstr(verbose));
+	if(verbose) printf("%s: end function (from %s, to %s, shift %d, verbose %s)\n", __func__, in, out, shift, boolstr(verbose));
 	if(in_obj  != stdin)  fclose(in_obj);
 	if(out_obj != stdout) fclose(out_obj);
 
@@ -105,7 +108,7 @@ void rotate(const char *in, const char *out, short shift, bool verbose)
 // User Interface functions
 void version(void)
 {
-	printf("rot (%s): a customizable rotation cipher\n", VERSION);
+	printf("ROTate (%s): a customizable substitution cipher\n", VERSION);
 	return;
 }
 
@@ -126,14 +129,101 @@ void usage(void)
 // Main function
 int main(int argc, char *argv[])
 {
-	if(argc < 2)
+	// This program needs some sort of input, whether it be from pipe or a file
+	bool frompipe = !isatty(STDIN_FILENO);
+	if(!frompipe && argc < 2)
 	{
-		fprintf(stderr, "%s: too few arguments, try \"--help\"\n", argv[0]);
+		fprintf(stderr, "%s: too few arguments. try \"--help\"\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	const char *program = argv[0];
+	char infile[bufsize] = {0}, outfile[bufsize] = {0};
+	char *program = argv[0];
+	int c, shift_amount = 0;
 
-	printf("hello world!\n");
+	bool verbose_flag = false;
+
+	// Iterate through all arguments sent, character by character
+	while(--argc > 0 && (*++argv)[0] != '\0')
+	{
+		if((*argv)[0] != '-' && !my_isdigit((*argv)[0]))
+		{
+			if(!strempty(outfile) && !my_isdigit((*argv)[0]))
+			{
+				fprintf(stderr, "%s: discarded program input -- \"%s\"\n", program, *argv);
+				continue;
+			}
+
+			if	(strempty(infile) && !frompipe) strncpy(infile, *argv, bufsize);
+			else if (strempty(outfile))		strncpy(outfile, *argv, bufsize);
+		}
+
+		if((*argv)[0] == '-')
+		{
+			// If there's another dash, then it's a long option.
+			// Move the pointer up 2 places and compare the word itself.
+			if((*argv)[1] == '-')
+			{
+				// Using continue statements here so that the user
+				// can use both single character and long options
+				// simultaniously, and the loop can test both.
+				if(strcmp((*argv) + 2, "help")    == 0) { usage();   exit(EXIT_SUCCESS); }
+				if(strcmp((*argv) + 2, "version") == 0) { version(); exit(EXIT_SUCCESS); }
+				if(strcmp((*argv) + 2, "verbose") == 0) { verbose_flag = true; continue; }
+				if(strcmp((*argv) + 2, "num")     == 0)
+				{
+					if(*(argv + 1) == nullptr)
+					{
+						fprintf(stderr, "%s: option \"%s\" requires argument\n", program, *argv);
+						exit(EXIT_FAILURE);
+					}
+					else shift_amount = atoi(*(argv + 1));
+					continue;
+				}
+			}
+			while((c = *++argv[0]))
+			{
+				// Single character option testing here.
+				switch(c)
+				{
+					case 'h': usage(); exit(EXIT_SUCCESS);
+					case 'v': verbose_flag = true; break;
+					case 'n':
+						if(*(argv + 1) == nullptr)
+						{
+							fprintf(stderr, "%s: option \"%c\" requires argument\n", program, c);
+							exit(EXIT_FAILURE);
+						}
+						else shift_amount = atoi(*(argv + 1));
+						break;
+					// This error flag can either be set by a
+					// completely unrelated character inputted,
+					// or you managed to put -option instead of
+					// --option.
+					default : fprintf(stderr, "%s: unknown option. try \"--help\"", program); exit(EXIT_FAILURE);
+				}
+			}
+
+			continue;
+		}
+	}
+
+	// Again let's as if we're getting anything from pipe, that takes precedence over
+	// input from a file
+	bool is_stdin  = frompipe && strempty(infile);
+	bool is_stdout = strempty(outfile);
+
+	// Debug prints if it's set
+	if(verbose_flag)
+	{
+		printf("%s: %s\n", program, is_stdin  ? "recieved input from pipe" : "recieved input from file");
+		printf("%s: %s\n", program, is_stdout ? "writing to stdout"	   : "writing to file");
+	}
+
+	// Actual rotation
+	rotate((is_stdin == true) ? nullptr : infile, (is_stdout == true) ? nullptr : outfile, shift_amount, verbose_flag);
+
+	// End!
 	exit(EXIT_SUCCESS);
 }
+
